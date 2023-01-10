15c5b12daf7155c64e3b97b6b64090c0a18a640d
EddieMendez <eddiemendez82@gmail.com>
2023-01-10 09:31:15 -0800

    adding js to file

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Dodger Stadium",
+    link: "https://unsplash.com/@henrypaulphotography?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+  },
+  {
+    name: "Yankee Stadium",
+    link: "https://unsplash.com/@chanan?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+  },
+  {
+    name: "Fenway Park",
+    link: "https://unsplash.com/@fotowei?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+  },
+  {
+    name: "Wrigley Field",
+    link: "https://unsplash.com/@ryanarnst?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+  },
+  {
+    name: "Oracle Park",
+    link: "https://unsplash.com/@ruaribell?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+  },
+  {
+    name: "PNC Park",
+    link: "https://unsplash.com/@jcpeacock?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+  },
+];
