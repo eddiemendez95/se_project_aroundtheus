ee26917d93d9293c6b4f0f8267ac60334cfa24ce
EddieMendez <eddiemendez82@gmail.com>
2023-01-19 12:14:32 -0800

    edit: mobile view of modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,32 @@
 const initialCards = [
   {
-    name: "Dodger Stadium",
-    link: "https://unsplash.com/@henrypaulphotography?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
   {
-    name: "Yankee Stadium",
-    link: "https://unsplash.com/@chanan?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
   {
-    name: "Fenway Park",
-    link: "https://unsplash.com/@fotowei?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
   {
-    name: "Wrigley Field",
-    link: "https://unsplash.com/@ryanarnst?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
   {
-    name: "Oracle Park",
-    link: "https://unsplash.com/@ruaribell?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
   {
-    name: "PNC Park",
-    link: "https://unsplash.com/@jcpeacock?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
   },
 ];
+
+//                              Elements                                      //
+//                                                                            //
 
 const openEditModalButton = document.querySelector(".profile__edit-button");
 const popupElement = document.querySelector(".modal");
@@ -42,17 +45,34 @@ const profileDescriptionInput = document.querySelector(
 
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
-const cardListEl = document.querySelector(".card__list");
 
-function closePopup() {
-  popupElement.classList.remove("modal_opened");
-}
+const cardListElement = document.querySelector(".cards__list");
+
+//                                Functions                               //
+//                                                                        //
 
 function openPopup() {
   popupElement.classList.add("modal_opened");
 }
 
-function getCardElement(data) {}
+function closePopup() {
+  popupElement.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageElement = cardElement.querySelector(".card__image");
+  const cardTitleElement = cardElement.querySelector(".card__description-name");
+
+  cardImageElement.src = cardData.link;
+  cardImageElement.alt = cardData.name;
+  cardTitleElement.textContent = cardData.name;
+
+  return cardElement;
+}
+
+//                            Event Listener                                //
+//                                                                         //
 
 openEditModalButton.addEventListener("click", openPopup);
 {
@@ -73,24 +93,8 @@ profileEditForm.addEventListener("submit", function (event) {
   closePopup();
 });
 
-function getCardElement(cardData) {
-  //let cardElement = cardTemplate.clonenode(true);
-  //let cardText = cardElement.querySelector(".card__text");
-  //let cardImage = cardElement.querySelector(".card__image");
-
-  const cardTemplate = cardTemplate.clonenode(true);
-  const cardImageEl = cardElement.querySelector("card__image");
-  const cardTitleEl = cardElement.querySelector("card__title");
-
-  cardImage.src = data.link;
-  cardImage.alt = data.link;
-  cardText.textContent = data.name;
-
-  cardTitleEl.textContent = cardData.name;
-  return cardElement;
-}
-
 initialCards.forEach((cardData) => {
   const cardElement = getCardElement(cardData);
-  cardListEl.prepend(cardElement);
+
+  cardListElement.append(cardElement);
 });
